657cf3202f233acc4e5a3e1f31496646da5dfe9b
Magnus Auvinen <magnus.auvinen@gmail.com>
2008-08-17 08:52:24 +0000

    continued with clean up

--- src/game/g_version.hpp
+++
@@ -1,4 +0,0 @@
-/* copyright (c) 2007 magnus auvinen, see licence.txt for more info */
-#include "generated/nethash.c"
-#define GAME_VERSION "0.5.0-dev"
-#define GAME_NETVERSION "0.5 " GAME_NETVERSION_HASH
