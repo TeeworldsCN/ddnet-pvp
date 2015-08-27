teerace/racemod

saving server settings within the map and loading them on init

src/engine/map.h
@@ -11,6 +11,7 @@ class IMap : public IInterface
 public:
 	virtual void *GetData(int Index) = 0;
 	virtual void *GetDataSwapped(int Index) = 0;
+	virtual int GetUncompressedDataSize(int Index) = 0;
 	virtual void UnloadData(int Index) = 0;
 	virtual void *GetItem(int Index, int *Type, int *pID) = 0;
 	virtual void GetType(int Type, int *pStart, int *pNum) = 0;

src/engine/shared/map.cpp
@@ -13,6 +13,7 @@ public:
 
 	virtual void *GetData(int Index) { return m_DataFile.GetData(Index); }
 	virtual void *GetDataSwapped(int Index) { return m_DataFile.GetDataSwapped(Index); }
+	virtual int GetUncompressedDataSize(int Index) { return m_DataFile.GetUncompressedDataSize(Index); }
 	virtual void UnloadData(int Index) { m_DataFile.UnloadData(Index); }
 	virtual void *GetItem(int Index, int *pType, int *pID) { return m_DataFile.GetItem(Index, pType, pID); }
 	virtual void GetType(int Type, int *pStart, int *pNum) { m_DataFile.GetType(Type, pStart, pNum); }

TeeworldsCN/ddnet-pvp

src/engine/map.h
@@ -14,6 +14,7 @@ public:
 	virtual void *GetDataSwapped(int Index) = 0;
 	virtual void UnloadData(int Index) = 0;
 	virtual void *GetItem(int Index, int *Type, int *pID) = 0;
+	virtual int GetItemSize(int Index) = 0;
 	virtual void GetType(int Type, int *pStart, int *pNum) = 0;
 	virtual void *FindItem(int Type, int ID) = 0;
 	virtual int NumItems() = 0;

src/engine/shared/map.cpp
@@ -16,6 +16,7 @@ public:
 	virtual void *GetDataSwapped(int Index) { return m_DataFile.GetDataSwapped(Index); }
 	virtual void UnloadData(int Index) { m_DataFile.UnloadData(Index); }
 	virtual void *GetItem(int Index, int *pType, int *pID) { return m_DataFile.GetItem(Index, pType, pID); }
+	virtual int GetItemSize(int Index) { return m_DataFile.GetItemSize(Index); }
 	virtual void GetType(int Type, int *pStart, int *pNum) { m_DataFile.GetType(Type, pStart, pNum); }
 	virtual void *FindItem(int Type, int ID) { return m_DataFile.FindItem(Type, ID); }
 	virtual int NumItems() { return m_DataFile.NumItems(); }