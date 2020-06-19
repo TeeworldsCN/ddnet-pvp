TeeworldsCN/ddnet-pvp

commit d3565adbb9c4bd58f4099549584a25c27696738b

Allow larger map names

src/engine/demo.h
@@ -4,6 +4,7 @@
 #define ENGINE_DEMO_H
 
 #include <base/hash.h>
+#include <engine/map.h>
 #include <engine/shared/uuid_manager.h>
 #include "kernel.h"
 
@@ -29,7 +30,7 @@ struct CDemoHeader
 	unsigned char m_aMarker[7];
 	unsigned char m_Version;
 	char m_aNetversion[64];
-	char m_aMapName[64];
+	char m_aMapName[MAX_MAP_LENGTH];
 	unsigned char m_aMapSize[4];
 	unsigned char m_aMapCrc[4];
 	char m_aType[8];
@@ -45,7 +46,7 @@ struct CTimelineMarkers
 
 struct CMapInfo
 {
-	char m_aName[128];
+	char m_aName[MAX_MAP_LENGTH];
 	SHA256_DIGEST m_Sha256;
 	int m_Crc;
 	int m_Size;

src/engine/map.h
@@ -6,6 +6,11 @@
 #include <base/hash.h>
 #include "kernel.h"
 
+enum
+{
+	MAX_MAP_LENGTH = 128
+};
+
 class IMap : public IInterface
 {
 	MACRO_INTERFACE("map", 0)

src/game/server/gamecontroller.h
@@ -4,6 +4,7 @@
 #define GAME_SERVER_GAMECONTROLLER_H
 
 #include <base/vmath.h>
+#include <engine/map.h>
 
 class CDoor;
 #if !defined(_MSC_VER) || _MSC_VER >= 1600
@@ -54,7 +55,7 @@ protected:
 
 	void ResetGame();
 
-	char m_aMapWish[128];
+	char m_aMapWish[MAX_MAP_LENGTH];
 
 
 	int m_RoundStartTick;

commit 1f1d2f315686b90c9b1a4fbb3b2bf64c16520ec3

Fix demos header (fixes #2278)
Initially broken in d3565adbb9c4bd58f4099549584a25c27696738b

src/engine/demo.h
@@ -30,7 +30,7 @@ struct CDemoHeader
 	unsigned char m_aMarker[7];
 	unsigned char m_Version;
 	char m_aNetversion[64];
-	char m_aMapName[MAX_MAP_LENGTH];
+	char m_aMapName[64];
 	unsigned char m_aMapSize[4];
 	unsigned char m_aMapCrc[4];
 	char m_aType[8];