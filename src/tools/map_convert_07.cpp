TeeworldsCN/ddnet-pvp

commit d385b0ce8d34b8198f36e4f57a461111ece61433

Fix clang-analyzer-deadcode.DeadStores
/media/ddnet/src/game/client/components/statboard.cpp:288:3: warning: Value stored to 'tw' is never read [clang-analyzer-deadcode.DeadStores]

src/tools/map_convert_07.cpp
@@ -222,7 +222,6 @@ int main(int argc, const char **argv)
 	int i = 0;
 	for(int Index = 0; Index < g_DataReader.NumItems(); Index++)
 	{
-		pItem = g_DataReader.GetItem(Index, &Type, &ID);
 		if(Type == MAPITEMTYPE_IMAGE)
 			g_aImageIDs[i++] = Index;
 	}

src/base/system.c
@@ -2691,13 +2691,8 @@ int str_utf8_dist_buffer(const char *a_utf8, const char *b_utf8, int *buf, int b
 	dbg_assert(buf_len >= 2 * (a_utf8_len + 1 + b_utf8_len + 1), "buffer too small");
 	if(a_utf8_len > b_utf8_len)
 	{
-		int tmp1 = a_utf8_len;
 		const char *tmp2 = a_utf8;
-
-		a_utf8_len = b_utf8_len;
 		a_utf8 = b_utf8;
-
-		b_utf8_len = tmp1;
 		b_utf8 = tmp2;
 	}
 	a = buf;

src/game/mapbugs.cpp
@@ -27,7 +27,8 @@ static unsigned int BugToFlag(int Bug)
 {
 	unsigned int Result;
 	dbg_assert((unsigned)Bug < 8 * sizeof(Result), "invalid shift");
-	return Result = (1 << Bug);
+	Result = (1 << Bug);
+	return Result;
 }
 
 static unsigned int IsBugFlagSet(int Bug, unsigned int BugFlags)

commit 54d9e141170d44ecf5e55b92bd1799375117448c

Fix map_convert_07
Broken in https://github.com/ddnet/ddnet/commit/d385b0ce8d34b8198f36e4f57a461111ece61433#diff-17a71d0838a3f107d2baf573a0885c3b7727266b432a13836525737e83a81a58

Thanks to Jupstar for noticing

src/tools/map_convert_07.cpp
@@ -222,6 +222,7 @@ int main(int argc, const char **argv)
 	int i = 0;
 	for(int Index = 0; Index < g_DataReader.NumItems(); Index++)
 	{
+		g_DataReader.GetItem(Index, &Type, &ID);
 		if(Type == MAPITEMTYPE_IMAGE)
 			g_aImageIDs[i++] = Index;
 	}