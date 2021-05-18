f9d3f263523b18846ea7de9c35e49d3e792ef6d0
Freddie Wang <tsfreddiewang@gmail.com>
2021-05-19 00:11:49 +0800

    Change header of explodinglaser

--- src/game/server/weapons/explodinglaser.h
+++ src/game/server/weapons/explodinglaser.h
@@ -1,5 +1,5 @@
-#ifndef GAME_SERVER_WEAPONS_BOOSTLASER_H
-#define GAME_SERVER_WEAPONS_BOOSTLASER_H
+#ifndef GAME_SERVER_WEAPONS_EXPLODINGLASER_H
+#define GAME_SERVER_WEAPONS_EXPLODINGLASER_H
 
 #include "lasergun.h"
 
@@ -20,4 +20,4 @@ public:
 	static bool LaserHit(class CLaser *pLaser, vec2 HitPoint, CCharacter *pHit, bool OutOfEnergy);
 };
 
-#endif // GAME_SERVER_WEAPONS_BOOSTLASER_H
+#endif // GAME_SERVER_WEAPONS_EXPLODINGLASER_H
